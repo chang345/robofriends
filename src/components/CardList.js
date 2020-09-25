6d6c495d3237c1b1eb138089ce9f6e53ed88b84a
Sang Chang <sangchang@hotmail.com>
2020-09-24 20:38:58 -0500

    first commit

---
+++ src/components/CardList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+  if (true) {
+    throw new Error('N000001234');
+  }
+  return (
+    <div>
+      {robots.map((robot) => {
+        return (
+          <Card
+            key={robot.id}
+            id={robot.id}
+            name={robot.name}
+            email={robot.email}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default CardList;
